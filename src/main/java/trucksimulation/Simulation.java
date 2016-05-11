7c8110899c742f299bd2b64707262a122bd74486
Rocco Schulz <rocco.schulz@adesso.de>
2016-05-11 13:56:32 +0200

    delegate start and stop to the simulation object.

--- src/main/java/trucksimulation/Simulation.java
+++ src/main/java/trucksimulation/Simulation.java
@@ -8,15 +8,29 @@ import java.util.Map;
 import io.vertx.core.CompositeFuture;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import trucksimulation.routing.Route;
 import trucksimulation.traffic.TrafficIncident;
+import trucksimulation.trucks.DestinationArrivedException;
 import trucksimulation.trucks.Truck;
 
 /**
- * Representation of runtime information for a simulation.
- *
+ * Simulation representation.
  */
 public class Simulation {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(TruckControllerVerticle.class);
+	
+	private String id;
+	private Vertx vertx;
+	private Map<String, Truck> route2truckMap = new HashMap<>();
+	private List<Long> timerIds = new ArrayList<>();
+	
+	private int truckCount;
+	private int incidentCount;
+	private Future<Boolean> allRoutesLoaded = Future.future();
+	private Future<Boolean> allIncidentsAssigned = Future.future();
 	
 	public Simulation(String simulationId) {
 		this.id = simulationId;
@@ -28,20 +42,47 @@ public class Simulation {
 		this.vertx = vertx;
 	}
 	
-	private String id;
-	private Vertx vertx;
-	private Map<String, Truck> route2truckMap = new HashMap<>();
-	private List<Long> timerIds = new ArrayList<>();
-	
-	private int truckCount;
-	private int incidentCount;
-	private Future allRoutesLoaded = Future.future();
-	private Future allIncidentsAssigned = Future.future();
-	
+	/**
+	 * Starts the simulation as soon as all routes and incidents have been loaded.
+	 * 
+	 * Make sure to set the expected number of trucks and incidents and all incident,
+	 * truck and route instances, otherwise the simulation won't start.
+	 * 
+	 * @see #setTruckCount(int)
+	 * @see #addTruck(Truck)
+	 * @see #setIncidentCount(int)
+	 * @see #addRoute(String, Route)
+	 * @see #addTrafficIncident(TrafficIncident, List)
+	 */
 	public void start() {
 		CompositeFuture.all(allRoutesLoaded, allIncidentsAssigned).setHandler(h -> {
-			//TODO: start simulation, store timer ids
+			for(Truck truck : route2truckMap.values()) {
+				//FIXME: multiple trucks may drive the same route, needs different data structure
+				startMoving(truck);
+			}
 		});
+	}
+	
+	/**
+	 * Sets a periodic timer which moves the truck in each interval.
+	 * 
+	 * @param t truck to be moved
+	 * @return id of the timer, so that it can be cancelled
+	 */
+	private long startMoving(Truck t) {
+		long tId = vertx.setPeriodic(1000, timerId -> {
+			try {
+				t.move();
+				vertx.eventBus().publish("trucks", t.getJsonData());
+			} catch(DestinationArrivedException ex) {
+				LOGGER.info("Truck has arrived at destination: #" + t.getId());
+				vertx.cancelTimer(timerId);
+			} catch (Exception ex) {
+				LOGGER.error("Unexpected error, stopping truck #" + t.getId(), ex);
+				vertx.cancelTimer(timerId);
+			}
+		});
+		return tId;
 	}
 	
 	public void stop() {
@@ -59,38 +100,38 @@ public class Simulation {
 	}
 	
 	public void removeTruck(Truck truck) {
+		//FIXME: need different data structure
 		this.route2truckMap.remove(truck.getRouteId());
 	}
 	
 	/**
-	 * Assigns a traffic incident object to all trucks that are specified in the lsit of ids.
-	 * The caller must make sure that the mapping from incident to trucks is correct as no
+	 * Assigns a traffic incident object to all trucks which drive on one of the specified routes.
+	 * The caller must make sure that the mapping from incident to route is correct as no
 	 * additional checks are performed in the simulation object.
 	 * 
-	 * Throws an IllegalStateException if trucks have not been added yet and an IllegalArgumentException
-	 * if the list contains an ID which cannot be found in the list of trucks.
+	 * Operation is performed as soon as all routes have been assigned to the simulation trucks.
 	 * 
-	 * Incidents can only be assigned when
+	 * @see #getAllRoutesLoaded()
+	 * @see #addRoute(String, Route)
 	 * 
 	 * @param incident
 	 * @param truckIds ids of all trucks which are affected by the traffic incident
 	 */
 	public void addTrafficIncident(TrafficIncident incident, List<String> routeIds) {
-		if(route2truckMap.isEmpty()) {
-			throw new IllegalStateException("Trucks have not been added to the simulation yet.");
-		}
-		for(String routeId : routeIds) {
-			Truck truck = route2truckMap.get(routeId);
-			if(truck != null) {
-				truck.addTrafficIncident(incident);
-			} else {
-				throw new IllegalArgumentException("Truck with route id " + routeId + " not found in simulation.");
+		allRoutesLoaded.setHandler(h -> {
+			for(String routeId : routeIds) {
+				Truck truck = route2truckMap.get(routeId);
+				if(truck != null) {
+					truck.addTrafficIncident(incident);
+				} else {
+					throw new IllegalArgumentException("Truck with route id " + routeId + " not found in simulation.");
+				}
 			}
-		}
-		incidentCount--;
-		if(incidentCount == 0) {
-			allIncidentsAssigned.complete();
-		}
+			incidentCount--;
+			if(incidentCount == 0) {
+				allIncidentsAssigned.complete();
+			}
+		});
 	}
 
 	public String getId() {
@@ -125,7 +166,7 @@ public class Simulation {
 		this.timerIds = timerIds;
 	}
 	
-	public Future getAllRoutesLoaded() {
+	public Future<Boolean> getAllRoutesLoaded() {
 		return allRoutesLoaded;
 	}
 
--- src/main/java/trucksimulation/TruckControllerVerticle.java
+++ src/main/java/trucksimulation/TruckControllerVerticle.java
@@ -36,11 +36,6 @@ public class TruckControllerVerticle extends AbstractVerticle {
 	 * Maps simulation id's to the running state of the simulation (true when running).
 	 */
 	private LocalMap<String, Boolean> simulationStatus;
-	/**
-	 * Maps simulation id's to a list of periodic timers that are currently running as part of the simulation.
-	 */
-	@Deprecated
-	private Map<String, ArrayList<Long>> simTimerMap = new HashMap<String, ArrayList<Long>>();
 	
 	private HashMap<String, Simulation> simulations = new HashMap<String, Simulation>();
 	
@@ -71,7 +66,8 @@ public class TruckControllerVerticle extends AbstractVerticle {
 			msg.fail(400, "Simulation is already running.");
 			return;
 		}
-		simulations.put(simId, new Simulation(simId, vertx));
+		Simulation simulation = new Simulation(simId, vertx);
+		simulations.put(simId, simulation);
 		
 		JsonObject trucksQuery = new JsonObject().put("simulation", simId);
 		mongo.find("trucks", trucksQuery, res -> {
@@ -84,26 +80,43 @@ public class TruckControllerVerticle extends AbstractVerticle {
 				for(JsonObject truckJson : res.result()) {
 					Truck truck = new Truck(truckJson.getString("_id"));
 					truck.setRouteId(truckJson.getString("route"));
-					assignRoute(truck, truckJson.getString("route"));
+					assignRoute(simId, truck);
 				}
+				loadTrafficIncidents(simId);
+				simulation.start();
 			}
-		});		
+		});
+	}
+	
+	/**
+	 * Stops the simulation if it is running in this verticle and updates the running status in the shared status map {@link #simulationStatus}.
+	 * 
+	 * @param msg a JsonObject with an "_id" field containing the id string fo the simulation.
+	 */
+	private void stopSimulation(Message<JsonObject> msg) {
+		String simId = msg.body().getString("id");
+		if(simulations.containsKey(simId)) {
+			Simulation simulation = simulations.get(simId);
+			simulation.stop();
+			simulations.remove(simId);
+			setRunningStatus(simId, false);
+		}
 	}
 
 
 	/**
-	 * Resolves references to the trucks route and checks if traffic incidents are on the route.
+	 * Resolves reference to the truck's route and assigns route objects to the truck.
 	 * 
 	 * @param simId
 	 * @param truck
 	 */
-	private void assignRoute(Truck truck, String routeId) {
+	private void assignRoute(String simulationId, Truck truck) {
 		Gson gson = Serializer.get();
-		JsonObject routeQuery = new JsonObject().put("_id", routeId);
+		JsonObject routeQuery = new JsonObject().put("_id", truck.getRouteId());
 		
 		mongo.findOne("routes", routeQuery, new JsonObject(), r -> {
 			Route route = gson.fromJson(r.result().toString(), Route.class);
-			truck.setRoute(route);
+			simulations.get(simulationId).addRoute(truck.getRouteId(), route);
 		});
 	}
 	
@@ -114,31 +127,30 @@ public class TruckControllerVerticle extends AbstractVerticle {
 	 * 
 	 * @param simId the simulation id
 	 */
-	@SuppressWarnings("unchecked")
 	private void loadTrafficIncidents(String simId) {
 		Gson gson = Serializer.get();
 		JsonObject query = new JsonObject().put("simulation", simId);
-		
 		Simulation simulation = simulations.get(simId);
-		simulation.getAllRoutesLoaded().setHandler(h -> {
-			mongo.find("traffic", query, trafficResult -> {
-				if(trafficResult.result() != null) {
-					simulation.setIncidentCount(trafficResult.result().size());
-					
-					for(JsonObject incidentJson : trafficResult.result()) {
-						JsonObject intersectionQuery = buildIntersectionQuery(incidentJson, simId);
-						FindOptions idFieldOnly = new FindOptions().setFields(new JsonObject().put("_id", true));
-						
-						mongo.findWithOptions("routes", intersectionQuery, idFieldOnly, routes -> {
-							if(routes.result() != null && !routes.result().isEmpty()) {
-								TrafficIncident trafficIncident = gson.fromJson(incidentJson.toString(), TrafficIncident.class);
-								List<String> routeIds = routes.result().stream().map(c -> c.getString("_id")).collect(Collectors.toList());
-								simulation.addTrafficIncident(trafficIncident, routeIds);
-							}
-						});
-					}
+
+		mongo.find("traffic", query, trafficResult -> {
+			if (trafficResult.result() != null) {
+				simulation.setIncidentCount(trafficResult.result().size());
+
+				for (JsonObject incidentJson : trafficResult.result()) {
+					JsonObject intersectionQuery = buildIntersectionQuery(incidentJson, simId);
+					FindOptions idFieldOnly = new FindOptions().setFields(new JsonObject().put("_id", true));
+
+					mongo.findWithOptions("routes", intersectionQuery, idFieldOnly, routes -> {
+						if (routes.result() != null && !routes.result().isEmpty()) {
+							TrafficIncident trafficIncident = gson.fromJson(incidentJson.toString(),
+									TrafficIncident.class);
+							List<String> routeIds = routes.result().stream().map(c -> c.getString("_id"))
+									.collect(Collectors.toList());
+							simulation.addTrafficIncident(trafficIncident, routeIds);
+						}
+					});
 				}
-			});
+			}
 		});
 	}
 	
@@ -151,25 +163,12 @@ public class TruckControllerVerticle extends AbstractVerticle {
 		return query;
 	}
 	
-	private void registerTimer(String simulationId, long timerId) {
-		if(simTimerMap.get(simulationId) == null) {
-			simTimerMap.put(simulationId, new ArrayList<Long>());
-		}
-		simTimerMap.get(simulationId).add(timerId);
-	}
-	
-	private void stopSimulation(Message<JsonObject> msg) {
-		String simId = msg.body().getString("_id");
-		setRunningStatus(simId, false);
-		if(simTimerMap.containsKey(simId)) {
-			List<Long> timers = simTimerMap.get(simId);
-			for(long timer : timers) {
-				vertx.cancelTimer(timer);
-			}
-			simTimerMap.remove(simId);
-		}
-	}
-	
+	/**
+	 * Sets the status of the simulation so that all local verticles can see it.
+	 * 
+	 * @param simulationId
+	 * @param status
+	 */
 	private void setRunningStatus(String simulationId, boolean status) {
 		simulationStatus.put(simulationId, status);
 	}
@@ -179,6 +178,10 @@ public class TruckControllerVerticle extends AbstractVerticle {
 		return isRunning != null && isRunning.booleanValue() == true;
 	}
 	
+	/**
+	 * @deprecated should be moved into a bootstrap verticle which can be called separately
+	 */
+	@Deprecated
 	private void createSimulationData() {
 		Gson gson = new Gson();		
 		Position factoryStuttgart = new Position(48.772510, 9.165465);
@@ -208,9 +211,10 @@ public class TruckControllerVerticle extends AbstractVerticle {
 	}
 
 	/**
-	 * 
+	 * @deprecated should be moved into a demo verticle which can be run instead of this one when the server is started in demo mode
 	 * @param num number of trucks to simulate
 	 */
+	@Deprecated
 	private void startDemoSimulation(int num) {
 		mongo.find("cities", new JsonObject(), res -> {
 				int numTrucks = num;
@@ -221,6 +225,11 @@ public class TruckControllerVerticle extends AbstractVerticle {
 		});
 	}
 	
+	/**
+	 * @deprecated should be moved into a demo verticle which can be run instead of this one when the server is started in demo mode
+	 * @param num number of trucks to simulate
+	 */
+	@Deprecated
 	private void createTrucksAndRoutes(int num, List<JsonObject> cities) {
 		Random rand = new Random();
 		Gson gson = new Gson();
@@ -242,8 +251,8 @@ public class TruckControllerVerticle extends AbstractVerticle {
 						Route r = gson.fromJson(rpl.result().body(), Route.class);
 						Truck t = Truck.buildTruck();
 						t.setRoute(r);
-						startMoving(t);
-						LOGGER.info("Starting truck " + t.getId());
+						//FIXME: should use Simulation class
+						//startMoving(t);
 					}
 				});
 				num--;
@@ -251,26 +260,4 @@ public class TruckControllerVerticle extends AbstractVerticle {
 		}
 	}
 	
-	/**
-	 * Sets a periodic timer which moves the truck in each interval.
-	 * 
-	 * @param t truck to be moved
-	 * @return id of the timer, so that it can be cancelled
-	 */
-	private long startMoving(Truck t) {
-		long tId = vertx.setPeriodic(intervalMS, timerId -> {
-			try {
-				t.move();
-				vertx.eventBus().publish("trucks", t.getJsonData());
-			} catch(DestinationArrivedException ex) {
-				LOGGER.info("Truck has arrived at destination: #" + t.getId());
-				vertx.cancelTimer(timerId);
-			} catch (Exception ex) {
-				LOGGER.error("Unexpected error, stopping truck #" + t.getId(), ex);
-				vertx.cancelTimer(timerId);
-			}
-		});
-		return tId;
-	}
-
 }
